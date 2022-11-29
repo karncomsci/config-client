02e37fef24d21f3eb59d9cc3ebaf9b0f975f4c3b
Aiyakarn Yambun <karncomsci26@gmail.com>
2022-11-29 19:23:38 +0700

    init config-client

---
+++ src/main/java/com/workshop/configclient/controller/MessageResource.java
@@ -0,0 +1,20 @@
+package com.workshop.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+@RequestMapping("/rest")
+public class MessageResource {
+    @Value("${message: Default Hello}")
+    private String message;
+
+    @GetMapping("/message")
+    public String message(){
+        return message;
+    }
+}
---
+++ src/test/java/com/workshop/configclient/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.configclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
